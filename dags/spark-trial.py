cfdcbf4c6859f5f5d5ca94fc3075c00c793f3887
atherin <andrew.c.therin@gmail.com>
2020-09-01 11:43:52 -0500

    Testing PVC

--- dags/spark-trial.py
+++ dags/spark-trial.py
@@ -17,7 +17,7 @@ volume_mount = VolumeMount(
 )
 volume_config = {
     'persistentVolumeClaim': {
-        'claimName': 'airflow'
+        'claimName': 'airflow-dags'
     }
 }
 volume = Volume(name='persist-disk', configs=volume_config)
