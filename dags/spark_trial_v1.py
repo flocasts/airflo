5f7e01595e1b56c7f61d4e24f8d57f43c8b086c7
atherin <andrew.c.therin@gmail.com>
2020-09-02 17:08:34 -0500

    Testing PVC

--- dags/spark_trial_v1.py
+++ dags/spark_trial_v1.py
@@ -8,7 +8,7 @@ from airflow.contrib.kubernetes.volume_mount import VolumeMount
 aws_access_key_id = Secret('env', 'AWS_ACCESS_KEY_ID', 'airflow-aws', 'AWS_ACCESS_KEY_ID')
 aws_secret_access_key = Secret('env', 'AWS_SECRET_ACCESS_KEY', 'airflow-aws', 'AWS_SECRET_ACCESS_KEY')
 aws_account = Secret('env', 'AWS_ACCOUNT', 'airflow-aws', 'AWS_ACCOUNT')
-spark_image =
+spark_image = 'gcr.io/engineering-sandbox-228018/dev-airflow:1.10.12'
 
 volume_mount = VolumeMount(
     'persist-disk',
