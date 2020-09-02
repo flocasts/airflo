e83121f4f9f4aa453fa73d5f6d29206649a063bb
atherin <andrew.c.therin@gmail.com>
2020-09-02 16:21:10 -0500

    Testing PVC

--- dags/example_bash_operator.py
+++ dags/example_bash_operator.py
@@ -15,60 +15,31 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+"""
+This dag only runs some simple tasks to test Airflow's task execution.
+"""
+from datetime import datetime, timedelta
 
-"""Example DAG demonstrating the usage of the BashOperator."""
-
-from datetime import timedelta
-
-from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.models.dag import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
 
-args = {
+now = datetime.now()
+now_to_the_hour = (
+    now - timedelta(0, 0, 0, 0, 0, 3)
+).replace(minute=0, second=0, microsecond=0)
+START_DATE = now_to_the_hour
+DAG_NAME = 'test_dag_v1'
+
+default_args = {
     'owner': 'airflow',
+    'depends_on_past': True,
+    'start_date': days_ago(2)
 }
+dag = DAG(DAG_NAME, schedule_interval='*/10 * * * *', default_args=default_args)
 
-dag = DAG(
-    dag_id='example_bash_operator',
-    default_args=args,
-    schedule_interval='0 0 * * *',
-    start_date=days_ago(2),
-    dagrun_timeout=timedelta(minutes=60),
-    tags=['example']
-)
-
-run_this_last = DummyOperator(
-    task_id='run_this_last',
-    dag=dag,
-)
-
-# [START howto_operator_bash]
-run_this = BashOperator(
-    task_id='run_after_loop',
-    bash_command='echo 1',
-    dag=dag,
-)
-# [END howto_operator_bash]
-
-run_this >> run_this_last
-
-for i in range(3):
-    task = BashOperator(
-        task_id='runme_' + str(i),
-        bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        dag=dag,
-    )
-    task >> run_this
-
-# [START howto_operator_bash_template]
-also_run_this = BashOperator(
-    task_id='also_run_this',
-    bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
-    dag=dag,
-)
-# [END howto_operator_bash_template]
-also_run_this >> run_this_last
-
-if __name__ == "__main__":
-    dag.cli()
+run_this_1 = DummyOperator(task_id='run_this_1', dag=dag)
+run_this_2 = DummyOperator(task_id='run_this_2', dag=dag)
+run_this_2.set_upstream(run_this_1)
+run_this_3 = DummyOperator(task_id='run_this_3', dag=dag)
+run_this_3.set_upstream(run_this_2)
